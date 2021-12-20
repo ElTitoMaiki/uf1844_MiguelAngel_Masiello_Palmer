c30dfd136646dff73827605ab3d86f11f756f0de
ElTitoMaiki <aciruselvirus@gmail.com>
2021-12-20 13:09:03 +0100

    fg

--- sitio-node/app.js
+++ sitio-node/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('style.css'));
+
 app.get('/',function(request,res){
     res.sendFile('./index.html',{root:__dirname});
 });
@@ -13,3 +15,8 @@ app.listen('3000', function() {
 app.use(function (req, res, next) {
     res.status(404).sendFile(__dirname+'/404.html');
   })
+
+
+
+  
+  //https://github.com/ElTitoMaiki/uf1844_MiguelAngel_Masiello_Palmer/tree/master/sitio-node
